682a2e739cc9784c1a165df0eb6b5ceb252f1a1b
AbdurRehmanAfzal <67862582+AbdurRehmanAfzal@users.noreply.github.com>
2024-07-26 10:32:58 -0400

    added variable details

--- Python/day_1.py
+++ Python/day_1.py
@@ -1,5 +1,66 @@
-# this is my first comment
-"""this is multiline comment"""
+# # this is my first comment
+# """this is multiline comment"""
 
 
-# three formats are using in python 
+# """three formats are used in PYthon
+# 1- pascal e.g MyDeparment each word first leter should be in Capital
+# 2- camel e.g myDeparmentCenter
+# 3- snake e.g my_deparment_center  used function or method
+# """
+
+
+# # variable 
+
+# a = 5
+# print("varible type :", type(a))
+
+# # pytho is case sensetive
+# A = 6
+# print(a, A)
+# """int , float, string, bool, list, dic, tuple , set , complex, frozenset"""
+
+# s = 'abdul'
+# s_2 = "abdul2"
+# s_3 = """this is my first line 
+#     this is my second line
+# """
+
+# print('strings----', s, s_2, s_3)
+
+# print('type of this variable:  ', type(s))
+
+# # assign multiple varibale value in signle line
+
+# a, b, c = 5, 's', True
+# print(a, b, c)
+# c, d, e = {a: 5, b:6, c:10}
+# print(c, d, e)
+
+
+a = 5
+b = a
+
+a = 6
+
+print('a', id(a))
+
+print('b', id(b))
+
+print(a,b)
+print('a', id(a))
+
+print('b', id(b))
+
+# int a = 5;
+# int b = a;
+
+
+bool_var = True
+bool_var_2 = False
+
+"""
+start var with e,g letter, _ , combination of aplha, numeric, sign _
+
+"""
+
+
